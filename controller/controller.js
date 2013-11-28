d29ad820c654f4063da4f36deb359d3fb22c9cbd
maniakill <maniak_is_me@yahoo.com>
2013-11-28 21:44:36 +0200

    changes
    Made more modular by creating the footer and header controller, added
    the add and lists controller.

--- controller/controller.js
+++ controller/controller.js
@@ -2,9 +2,7 @@ var ctrl = angular.module('ctrl', []);
 
 ctrl.controller('start',['$scope', '$timeout', '$location',
     function ($scope,$timeout,$location){
-        var d = new Date(),
-            timed = d.getDate()+'/'+(d.getMonth()+1)+'/'+d.getFullYear();
-        var target = localStorage.token ? '/timesheet/'+timed : '/login';
+        var target = localStorage.token ? '/timesheet' : '/login';
         $timeout(function() {
             $location.path(target);
         }, 2000);
@@ -27,9 +25,7 @@ ctrl.controller('login',['$scope', '$http', '$templateCache','$location',
                     if(data.code == 'ok'){
                         localStorage.token = data.response;
                         localStorage.username = $scope.params['username'];
-                        var d = new Date(),
-                            timed = d.getDate()+'/'+(d.getMonth()+1)+'/'+d.getFullYear();
-                        $location.path('/timesheet/'+timed);
+                        $location.path('/timesheet');
                     }else{
                         // something went very wrong!!! (maybe script error)
                     }
@@ -42,11 +38,16 @@ ctrl.controller('login',['$scope', '$http', '$templateCache','$location',
     }
 ]);
 
-ctrl.controller('timesheet',['$scope', '$timeout','project', '$routeParams',
+ctrl.controller('timesheet',['$scope', '$timeout','project', '$routeParams', 
     function ($scope, $timeout ,project, $routeParams){
         $scope.projects = [];
-        var time = $routeParams.d+'/'+$routeParams.m+'/'+$routeParams.y;
-        project.getProjects(time, function(results) {
+        if($routeParams.y && $routeParams.m && $routeParams.d){ 
+            var time = $routeParams.d+'/'+$routeParams.m+'/'+$routeParams.y;
+        }else{
+            var time='';
+        }
+        
+        project.getTime(time).then(function(results){
             if(typeof(results.response.project) == 'object'){
                 $scope.projects = results.response.project;
                 for(x in $scope.projects){
@@ -55,22 +56,7 @@ ctrl.controller('timesheet',['$scope', '$timeout','project', '$routeParams',
                     }                
                 }
             }
-        });        
-    
-        $scope.today = function() {
-            $scope.dt = new Date($routeParams.y, $routeParams.m-1, $routeParams.d);            
-        };
-        $scope.today();
-
-        $scope.open = function() {
-            $timeout(function() {
-                $scope.opened = true;
-            });
-        };
-        $scope.dateOptions = {
-            'year-format': "'yy'",
-            'starting-day': 1
-        };
+        });
 
         function number2hour(number){
             var value = '0:00';
@@ -98,6 +84,156 @@ ctrl.controller('timesheet',['$scope', '$timeout','project', '$routeParams',
                 }
             }
             return value;
+        } 
+    }
+]);
+
+ctrl.controller('footer',['$scope', '$routeParams',
+    function ($scope, $routeParams){
+        var d = new Date();
+        $scope.timed = d.getDate()+'/'+(d.getMonth()+1)+'/'+d.getFullYear();
+    }
+]);
+
+ctrl.controller('header',['$scope', '$timeout', '$routeParams', '$location', '$route', '$modal', '$log',
+    function ($scope, $timeout, $routeParams,$location, $route, $modal, $log){
+        $scope.timesheet = true;
+        $scope.add_page = true;
+
+        switch($route.current.controller){
+            case 'timesheet':
+                $scope.timesheet = false;
+                break;
+            case 'add':
+                $scope.add_page = false;
+                break;
+            case 'lists':
+                $scope.add_page = false;
+                break;
+            default:
+                $scope.timesheet = false;
+                break;
         }
+
+        $scope.today = function() {
+            if($routeParams.y && $routeParams.m && $routeParams.d){ 
+                $scope.dt = new Date($routeParams.y, $routeParams.m-1, $routeParams.d);                
+            }else{
+                $scope.dt = new Date();                
+            }
+        };
+        $scope.today();
+
+        $scope.open = function() {
+            $timeout(function() {
+                $scope.opened = true;
+            });
+        };
+        $scope.dateOptions = {
+            'year-format': "'yy'",
+            'starting-day': 1
+        };
+
+        /*$scope.add = function(){
+            $location.path('/add');
+        }*/
+        
+        $scope.times = function(){
+            $location.path('/timesheet');   
+        }
+
+        /*modal*/
+        $scope.task_type = [ { title: 'Add Task for Project', url: '/lists' }, {title: 'Add Ad Hoc Task', url: '/lists/adhoc'} ];
+
+        $scope.add = function () {
+
+            var modalInstance = $modal.open({
+              templateUrl: 'layout/task_type.html',
+              controller: 'task_type',
+              resolve: {
+                items: function () {
+                  return $scope.task_type;
+                }
+              }
+            });
+
+            /*modalInstance.result.then(function (selectedItem) {
+              $scope.selected = selectedItem;
+            }, function () {
+              // $log.info('Modal dismissed at: ' + new Date());
+            });*/
+        };
+        /*modalend*/
+    }
+]);
+
+ctrl.controller('add',['$scope','$routeParams', 'project', '$location',
+    function ($scope, $routeParams, project, $location){
+        $scope.date = 'today';
+        $scope.customer = 'Customer Name';
+        $scope.project = 'Project Name';
+        $scope.task = 'Select Task';
+
+        if($routeParams.item){
+            var p = project.getProject($routeParams.item);
+            if(p){
+                $scope.project = p.project_name;
+                $scope.customer = p.customer_name;
+            }else{
+                $location.path('/timesheet');
+            }
+        }
+
+        $scope.today = function() {
+            if($routeParams.y && $routeParams.m && $routeParams.d){ 
+                $scope.dta = new Date($routeParams.y, $routeParams.m-1, $routeParams.d);
+            }else{
+                $scope.dta = new Date();
+            }
+        };
+        $scope.today();
+
+        $scope.open = function() {
+            $timeout(function() {
+                $scope.opened = true;
+            });
+        };
+        $scope.dateOptions = {
+            'year-format': "'yy'",
+            'starting-day': 1
+        };
+    }
+]);
+
+ctrl.controller('task_type',['$scope','$modalInstance','items', '$location',
+    function ($scope, $modalInstance, items, $location) {
+        $scope.items = items;
+        
+        $scope.open = function(url){
+            $location.path(url);
+            $scope.cancel();
+        }
+
+        $scope.cancel = function () {
+            $modalInstance.dismiss('cancel');
+        };
+    }
+
+]);
+
+ctrl.controller('lists',['$scope', '$http', '$location', 'project',
+    function ($scope, $http, $location, project ){
+        $scope.items = [];
+
+        project.getProjectList().then(function(results){
+            if(typeof(results.response[0].projects) == 'object'){
+                $scope.items = results.response[0].projects;               
+            }
+        });
+
+        $scope.open = function (item){
+            $location.path('/add/'+item)
+        }
+
     }
 ]);
--- js/timetracker.js
+++ js/timetracker.js
@@ -15,22 +15,67 @@ app.config(function ($routeProvider) {
                 controller: 'login',
                 templateUrl: 'layout/login.html'
             })
+        .when('/timesheet',{
+                controller: 'timesheet',
+                templateUrl: 'layout/timesheet.html'
+            })
         .when('/timesheet/:d/:m/:y',
             {
                 controller: 'timesheet',
                 templateUrl: 'layout/timesheet.html'
+            })
+        .when('/lists',
+            {
+                controller: 'lists',
+                templateUrl: 'layout/lists.html'
+            })
+        // .when('/add',
+        //     {
+        //         controller: 'add',
+        //         templateUrl: 'layout/add.html'
+        //     })
+        .when('/add/:item',
+            {
+                controller: 'add',
+                templateUrl: 'layout/add.html'
             })
         .otherwise({ redirectTo: '/' });
 });
 
 app.factory('project', ['$http','$templateCache',
     function ($http, $templateCache) {
-        return {
-            getProjects: function(time,callback) {
-                $http.get('https://go.salesassist.eu/pim/mobile/index.php?do=mobile-time&api_key='+localStorage.token+'&username='+localStorage.username+'&start='+time).success(callback);
-            }
-        };
+        var project = {},
+            url = 'https://go.salesassist.eu/pim/mobile/',
+            key = 'api_key='+localStorage.token+'&username='+localStorage.username,
+            project_list = [];
         
+        project.getTime = function(time) {
+            this.data = $http.get(url+'index.php?do=mobile-time&'+key+'&start='+time).then(function(response){
+                return response.data;
+            });
+            return this.data;
+        }
+
+        project.getProjectList = function(){
+            this.data = $http.get(url+'index.php?do=mobile-project_list&'+key).then(function(response){
+                if(typeof(response.data.response[0].projects) == 'object'){
+                   project_list = response.data.response[0].projects;               
+                }
+                return response.data;
+            });
+            return this.data;
+        }
+
+        project.getProject = function(id){
+            for (x in project_list) {
+                if (project_list[x].project_id === id) {
+                    return project_list[x];
+                }
+            }
+            return null;
+        }
+        
+        return project;
     }
 ]);
 
