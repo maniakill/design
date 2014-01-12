632fd9deb74c39e3ba75abb877240d9d42f639b1
maniakill <maniak_is_me@yahoo.com>
2014-01-12 14:56:21 +0200

    test 2

--- js/index.js
+++ js/index.js
@@ -44,6 +44,7 @@ var app = {
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
         alert('e');
+        $('#rrr').text('Received Event: ' + id);
         console.log('Received Event: ' + id);
     }
 };
