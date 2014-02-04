61aca1136066e87be900af50f2f1cb9b406dd940
maniakill <maniak_is_me@yahoo.com>
2014-02-04 23:12:24 +0200

    test

--- js/timetracker.js
+++ js/timetracker.js
@@ -36,7 +36,7 @@ function checkConnection() {
 }
 
 function capturePhoto() {
-    
+    alert('capturePhoto');
   // Take picture using device camera and retrieve image as base64-encoded string
     navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
     destinationType: destinationType.DATA_URL });
@@ -45,7 +45,7 @@ function capturePhoto() {
 function onPhotoDataSuccess(imageData) {
   // Uncomment to view the base64-encoded image data
   console.log(imageData);
-
+    alert('capturePhoto2');
   // Get image handle
   var smallImage = document.getElementById('smallImage');
 
