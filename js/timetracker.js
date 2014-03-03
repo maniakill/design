01e3b1734ae8e951a9699682721bded526fb693b
maniakill <maniak_is_me@yahoo.com>
2014-03-03 20:00:21 +0200

    d

--- js/timetracker.js
+++ js/timetracker.js
@@ -1,40 +1,6 @@
 /// <reference path="../Scripts/angular-1.1.4.js" />
-alert('11');
-var apps = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        alert('load');
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        alert('device ready');
-        // apps.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
-};
-apps.initialize();
-/*var deviceReady = false;
+alert('12');
+var deviceReady = false;
 var pictureSource;
 var destinationType;
 window.addEventListener('load', function() {
@@ -56,7 +22,6 @@ function onDeviceReady() {
 }
 
 function checkConnection() {
-    alert(deviceReady.toSource());
     if(deviceReady){
         var networkState = navigator.connection.type;
     }else{
@@ -74,7 +39,7 @@ function checkConnection() {
     // alert('Connection type: ' + states[networkState]);
     return networkState;
 }
-/*
+
 function capturePhoto() {
     // console.log(destinationType);
     alert('e '+Camera.DestinationType.DATA_URL);
@@ -127,7 +92,7 @@ function onPhotoURISuccess(imageURI) {
   //
   largeImage.src = imageURI;
 }
-*/
+
 // phoneGap
 var app = angular.module('timeT', ['ngRoute','ctrl','ui.bootstrap']);
 
