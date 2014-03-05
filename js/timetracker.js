d76967768773d575a8344ccfb367f19fc710f0c8
maniakill <maniak_is_me@yahoo.com>
2014-03-05 17:14:26 +0200

    test 2

--- js/timetracker.js
+++ js/timetracker.js
@@ -21,33 +21,33 @@ function checkConnection() {
     return networkState;
 }
 
-function capturePhoto() {
-    // console.log(destinationType,pictureSource);
-  // Take picture using device camera and retrieve image as base64-encoded string
-    navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
-     destinationType: destinationType.DATA_URL });
-}
+// function capturePhoto() {
+//     // console.log(destinationType,pictureSource);
+//   // Take picture using device camera and retrieve image as base64-encoded string
+//     navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
+//      destinationType: destinationType.DATA_URL });
+// }
 
-function onPhotoDataSuccess(imageData) {
-    console.log('onPhotoDataSuccess');
-  // Uncomment to view the base64-encoded image data
-  console.log(imageData);
+// function onPhotoDataSuccess(imageData) {
+//     console.log('onPhotoDataSuccess');
+//   // Uncomment to view the base64-encoded image data
+//   console.log(imageData);
 
-  // Get image handle
-  var smallImage = document.getElementById('smallImage');
+//   // Get image handle
+//   var smallImage = document.getElementById('smallImage');
 
-  // Unhide image elements
-  smallImage.style.display = 'block';
+//   // Unhide image elements
+//   smallImage.style.display = 'block';
 
-  // Show the captured photo
-  // The in-line CSS rules are used to resize the image
-  smallImage.src = "data:image/jpeg;base64," + imageData;
-}
+//   // Show the captured photo
+//   // The in-line CSS rules are used to resize the image
+//   smallImage.src = "data:image/jpeg;base64," + imageData;
+// }
 
-function onFail(message) {
-    console.log('onFail');
-    alert('Failed because: ' + message);
-}
+// function onFail(message) {
+//     console.log('onFail');
+//     alert('Failed because: ' + message);
+// }
 
 function getPhoto(source) {
     console.log('getPhoto');
