114be6181dd681f4249fc1bc97b6c62809dfabee
maniakill <maniak_is_me@yahoo.com>
2014-03-14 22:09:16 +0200

    86

--- js/timetracker.js
+++ js/timetracker.js
@@ -15,7 +15,7 @@ function checkConnection() {
 function capturePhoto() {
     // Take picture using device camera and retrieve image as base64-encoded string
     navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
-     destinationType: destinationType.DATA_URI });
+     destinationType: destinationType.DATA_URL });
 }
 
 function onPhotoDataSuccess(imageData) {
@@ -32,7 +32,7 @@ function onFail(message) {
 function getPhoto(source) {
     // Retrieve image file location from specified source
     navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50,
-    destinationType: destinationType.DATA_URI,
+    destinationType: destinationType.DATA_URL,
     sourceType: source });
 }
 function onPhotoURISuccess(imageURI) {
