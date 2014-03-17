d14dd9d658fcba93ed4032e231c238562ab8ff74
maniakill <maniak_is_me@yahoo.com>
2014-03-18 00:02:02 +0200

    sd

--- js/timetracker.js
+++ js/timetracker.js
@@ -483,7 +483,9 @@ app.factory('project', ['$http','$templateCache', '$location', '$rootScope', '$i
                     item.customer_id = pId;
                     item.customer_name = project.getCustomer(pId).customer_name;
                     item.sync = 1;
-                    item.picture = smallImage.src.search('data') > 0 ? smallImage.src : '';                    
+                    item.picture = smallImage.src.search('data') > 0 ? smallImage.src : '';
+                    alert(smallImage.src);
+                    alert(smallImage.src.search('data'));           
                     if(!t){
                         var d = new Date();
                         t = d.getDate()+'/'+(d.getMonth()+1)+'/'+d.getFullYear();
