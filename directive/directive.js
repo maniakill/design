0f88e9f9de65722ed6aaaef2e30b1231bd0ac987
maniakill <maniak_is_me@yahoo.com>
2014-03-20 20:30:59 +0200

    face

--- directive/directive.js
+++ directive/directive.js
@@ -2,20 +2,23 @@ app.animation('.my-crazy-animation', function() {
 return {
 enter: function(element, done) {
   console.log('enter');
-  element.css({'right': '-100%' }).removeClass('anim-leave').addClass('anim anim-enter').animate({ right: '0' },1000, done);
+  // element.css({'right': '-100%' }).removeClass('anim-leave').addClass('anim anim-enter').animate({ right: '0' },1000, done);
+  element.css({'opacity':'0'}).animate({'opacity':'1'},1000,done);
   
 //run the animation here and call done when the animation is complete
 return function(cancelled) {
   console.log('cancelled');
+
 //this (optional) function will be called when the animation
 //completes or when the animation is cancelled (the cancelled
 //flag will be set to true if cancelled).
 };
 },
 leave: function(element, done) { console.log('leave'); 
-  element.removeClass('anim-enter').addClass('anim anim-leave').animate({
-        left: '0'
-      },1000, done);
+  // element.removeClass('anim-enter').addClass('anim anim-leave').animate({
+  //       left: '0'
+  //     },1000, done);
+element.css({'opacity':'1'}).animate({'opacity':'0'},1000,done);
           },
 move: function(element, done) { console.log('move'); },
  
